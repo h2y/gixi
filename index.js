6a582010b6a987408fe24d92f247a76775db39a9
linuxenko <linuxenko@yahoo.com>
2016-03-12 23:13:37 +0200

    web and node module

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 
-module.exports =  require('./dist/gixi-min.js');
+module.exports =  require('./dist/gixi-module.js');
--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,13 @@ var webpack = require('webpack');
 var dir_js = path.resolve(__dirname, 'lib');
 var dir_build = path.resolve(__dirname, 'dist');
 
-module.exports = {
+
+
+module.exports = [{
     entry: path.resolve(dir_js, 'index.js'),
     output: {
         path: dir_build,
-        filename: 'gixi-min.js',
+        filename: 'gixi-module.js',
 	library : 'GIXI',
 	libraryTarget : 'commonjs2'
     },
@@ -37,4 +39,35 @@ module.exports = {
     },
     // Create Sourcemaps for the bundle
   //  devtool: 'source-map',
+},
+
+{
+    entry: path.resolve(dir_js, 'index.js'),
+    output: {
+        path: dir_build,
+        filename: 'gixi-min.js',
+    },
+    devServer: {
+        contentBase: dir_build,
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: dir_js,
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({minimize: true}),
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin()
+    ],
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+  //  devtool: 'source-map',
 }
+]
